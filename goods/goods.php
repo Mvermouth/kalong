024f11a0c6d44a57172743d0b8c262b9756416f8
kalong <498801877@qq.com>
2017-03-20 17:19:51 +0800

    20170320

--- goods/goods.php
+++ goods/goods.php
@@ -8,7 +8,7 @@
 define('ACC',true);
 require('./include/init.php');
 session_start();
-$goods_id=$_GET['goods_id'];
+$goods_id=$_GET['goods_id']+0;
 if(!$goods_id){
     echo "goods_id 不存在";
     header('location:index.php');
@@ -17,7 +17,7 @@ if(!$goods_id){
 $goods=new goodsModel();
 //得到商品
 //$sql="select * from goods where goods_id=".$goods_id;
-if(!$goods->find($goods_id)){
+if(!$g=$goods->find($goods_id)){
     echo 'kong';
     header('location:index.php');
     exit;
@@ -30,6 +30,9 @@ $cat_id=$goods->getDad($goods_id);
 $cats=new catModel();
 //面包屑
 $nav=array_reverse($cats->gettree($cat_id));
+//页头下拉
+$cat=new catModel();
+$catlist=$cat->getcatTree($cat->select(),0,0);
 
 
 include (ROOT.'./view/front/shangpin.html');
