070764340317ededb7dbb1f6eaf6712331ddd3ef
kalong <498801877@qq.com>
2017-03-21 17:15:40 +0800

    20170321

--- goods/tools/CartTool.class.php
+++ goods/tools/CartTool.class.php
@@ -39,5 +39,70 @@ class CartTool{
         return $_SESSION['cart'];
     }
     //添加商品
+    public function upCart($id,$goods_name,$shop_price,$num=1){
+        $item=array();
+        $item['name']=$goods_name;
+        $item['price']=$shop_price;
+        $item['num']=$num;
+        $this->items[$id]=$item;
+    }
+    //修改数量
+    //$id是商品主键
+    public function modNum($id,$num=1){
+        if(!$this->haveitem($id)){
+            return false;
+        }
+        $this->items[$id]['num']=$num;
+    }
+    //商品数量增加一
+    public function incNum($id){
+        if($this->haveitem($id)){
+            $this->items[$id]['num']+=1;
+        }
+    }
+    //-1
+    public function incNum($id){
+        if($this->haveitem($id)){
+            $this->items[$id]['num']-=1;
+        }
+        //if is 0 从购物车删除
+        if($this->items[$id]['num']<1){
+            $this->delItem($id);
+        }
+
+    }
+    //删除
+    public function delItem($id){
+        unset($this->items[$id]);
+    }
+
+
+
+
+
+    //判断是商品是否存在
+    public function haveitem($id){
+        return array_key_exists($id,$this->items);
+    }
+    //购物车商品种类
+    public function getInt(){
+        return count($this->items);
+    }
+
+    //清空购物车
+    public function clear(){
+        //$_SESSION['cart']=array();
+        $this->items=array();
+    }
 }
-print_r(CartTool::getCart());
+//CartTool::upCart(1,'kk',23.4,1);
+//print_r(CartTool::getCart());
+$car=CartTool::getCart();
+if($_GET['test']=='add'){
+    $car->upCart(1,'kk',23.4,1);
+    echo 'ok';
+}else if($_GET['test']=='cle'){
+    $car->clear();
+}else{
+    print_r($car);
+}
