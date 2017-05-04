aee229b8fcca14248636fd267516f1a1d415cc3a
kalon <498801877@qq.com>
2017-05-04 21:25:23 +0800

    3

--- mes/regist.js
+++ mes/regist.js
@@ -29,5 +29,29 @@ app.post("/doreg",(req,res)=>{
         })
     })
 })
+//执行登陆
+app.get("/login",(req,res)=>{
+    res.render("login");
+})
+
+app.post("/dologin",(req,res)=>{
+    var form = new formidable.IncomingForm();
+    form.parse(req,(err,fields)=>{
+        console.log(fields.name+fields.pass);
+        //数据库比较
+        db.find("user",{"name":fields.name},(err,result)=>{
+           if(result.length==1){
+               //var j=result[0].pass==md5(fields.pass) ? "1" : "-1";
+               res.send(result[0].pass==md5(fields.pass) ? "1" : "-1");
+           }else{
+               res.send("-2");
+           }
+
+            // console.log(result);
+            // console.log(result[0].pass);
+
+        })
+    })
+})
 
 app.listen(80);
